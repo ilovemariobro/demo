fd44af37d805d6f597d51aec64ad45f3cce1d72c
Mario Golub <94420233+radicwl@users.noreply.github.com>
2022-08-21 08:43:20 -0400

    Update landing.jsx

--- components/landing.jsx
+++ components/landing.jsx
@@ -3,6 +3,56 @@ import { Box, Typography, Fade, Link } from '@mui/material'
 function LandingContent() {
   return (
     <Fade appear in timeout={2500}>
+      <Box
+        width="100%"
+        height="400%"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          maxWidth: '100%',
+          margin: 'auto',
+          backgroundColor: 'rgba(0, 0, 0, 0.4)',
+        }}
+      >
+        <Box position="relative" textAlign="left" maxWidth="800px" padding={5}>
+          <Typography
+            variant="h1"
+            gutterBottom
+            color="#ffffff"
+            fontFamily="'Playfair Display', serif"
+          >
+          </Typography>
+          <Typography
+            variant="h4"
+            gutterBottom
+            color="#d3d3d3"
+            fontFamily="'Playfair Display', serif"
+          >
+            Melbourne, Australia
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            fontWeight="light"
+            color="#e5e5e5"
+            gutterBottom
+          >
+            I'm a software engineer specialising in building web application
+            using cloud technologies. Currently, I'm focused on creating
+            scalable and performant products at{' '}
+            <Link
+              href="https://www.blackmagicdesign.com/"
+              color="#ffffff"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Blackmagic Design
+            </Link>
+            .
+          </Typography>
+        </Box>
+      </Box>
     </Fade>
   )
 }
