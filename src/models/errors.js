641ab1cd5dffa0a0d56e93c2e93a1cad146ff3d5
Mario Golub <94420233+radicwl@users.noreply.github.com>
2022-08-20 20:16:00 -0400

    Add files via upload

---
+++ src/models/errors.js
@@ -0,0 +1,28 @@
+export const ErrCodeMethodNotAllowedError = 'MethodNotAllowedError'
+export const ErrCodeBadRequestError = 'BadRequestError'
+export const ErrCodeInternalServiceError = 'InternalServiceError'
+
+export const NewInternalServiceError = () => {
+  return {
+    code: ErrCodeInternalServiceError,
+    message: 'The service was unable to execute your request.',
+  }
+}
+
+export const NewMethodNotAllowedError = (method) => {
+  return {
+    code: ErrCodeMethodNotAllowedError,
+    message: `Method ${method} is not`,
+  }
+}
+
+export const NewBadRequestError = (reason) => {
+  return {
+    code: ErrCodeBadRequestError,
+    message: reason,
+  }
+}
+
+export const errResponse = ({ status, statusText }) => {
+  throw new Error(`${status}: ${statusText}`)
+}
