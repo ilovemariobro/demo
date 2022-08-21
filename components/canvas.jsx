a6f7b5ce3cc9dc4d3bb6d1888a3461c58dcbd883
Mario Golub <94420233+radicwl@users.noreply.github.com>
2022-08-21 08:28:57 -0400

    Update canvas.jsx

--- components/canvas.jsx
+++ components/canvas.jsx
@@ -23,7 +23,7 @@ function LandingContent() {
     <Fade appear in={showHeader} timeout={1000}>
       <Box
         width="100%"
-        height="100%"
+        height="1000%"
         display="flex"
         flexDirection="column"
         justifyContent="center"
@@ -120,7 +120,7 @@ export default function ThreeCanvas() {
   return win !== undefined ? (
     <Box
       width="100vw"
-      height="100vh"
+      height="1000vh"
       sx={{
         backgroundColor: 'transparent',
       }}
