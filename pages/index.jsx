4b92ceec42fe04bffee9dca25e9dde7f97c4afbc
Mario Golub <94420233+radicwl@users.noreply.github.com>
2022-08-21 12:03:00 -0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 import Head from 'next/head'
 
-import { Landing } from '../components/landing'
+import { Landing } from '../landing'
 import { useEffect, useMemo, useState } from 'react'
-import ThreeCanvas from '../components/canvas'
+import ThreeCanvas from '../canvas'
 
 const theme = createTheme({
   palette: {
